0da11f75c7bbf61228660a09182936477b4278bc
philip32469 <68779970+philip32469@users.noreply.github.com>
2022-03-17 20:11:23 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,60 +1,60 @@
-function initialize() {
-    var status = "* Offline *";
-    if (navigator.onLine) {
-        status = "* Online *";
-        retrieveContacts();
-    } else {
-        const localStorage = window.localStorage;
-        if (localStorage) {
-            const contacts = localStorage.getItem("contacts");
-            if (contacts) {
-                displayContacts(JSON.parse(contacts));
-            }
-        }
-    }
-    document.getElementById("status").innerHTML = status;
-
-    document.body.addEventListener("online", function () { document.getElementById("status").innerHTML = "Online"; }, false);
-    document.body.addEventListener("offline", function () { document.getElementById("status").innerHTML = "Offline"; }, false);
+function initialize(){
+	var status="* Offline *";
+	if(navigator.onLine){
+		status="* Online8947541516156 *";
+		retrieveContacts();
+	}else{
+		const localStorage = window.localStorage;
+		if(localStorage){
+			const contacts = localStorage.getItem("contacts");
+			if(contacts){
+				displayContacts(JSON.parse(contacts));
+			}
+		}
+	}
+	document.getElementById("status").innerHTML = status;
+	
+	document.body.addEventListener("online", function(){document.getElementById("status").innerHTML="Online";}, false);
+	document.body.addEventListener("offline", function(){document.getElementById("status").innerHTML="Offline";},false);
 }
 
-function retrieveContacts() {
-    const xhr = new XMLHttpRequest();
-    const url = "contacts.json";
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            var contacts = JSON.parse(xhr.response).contacts;
-            displayContacts(contacts);
-
-            const localStorage = window.localStorage;
-            if (localStorage) {
-                localStorage.setItem("contacts", JSON.stringify(contacts));
-            }
-        }
-    };
-
-    xhr.open("get", url);
-    xhr.send();
+function retrieveContacts(){
+	const xhr = new XMLHttpRequest();
+	const url = "contacts.json";
+	
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4){
+			var contacts = JSON.parse(xhr.response).contacts;
+			displayContacts(contacts);
+			
+			const localStorage = window.localStorage;
+			if(localStorage){
+				localStorage.setItem("contacts", JSON.stringify(contacts));
+			}
+		}
+	};
+	
+	xhr.open("get", url);
+	xhr.send();
 }
 
-function displayContacts(contacts) {
-    contacts.forEach(addRow);
+function displayContacts(contacts){
+	contacts.forEach(addRow);
 }
 
-function addRow(contact) {
-    var tcontent = document.getElementById("tcontent");
-    var row = tcontent.insertRow();
-
-    var nameCell = row.insertCell();
-    nameCell.setAttribute('data-label', "Name");
-    nameCell.innerHTML = contact.name;
-
-    var addressCell = row.insertCell();
-    addressCell.setAttribute('data-label', "Address");
-    addressCell.innerHTML = contact.address;
-
-    var emailCell = row.insertCell();
-    emailCell.setAttribute('data-label', "Email");
-    emailCell.innerHTML = contact.email;
+function addRow(contact){
+	var tcontent = document.getElementById("tcontent");
+	var row = tcontent.insertRow();
+	
+	var nameCell = row.insertCell();
+	nameCell.setAttribute('data-label', "Name");
+	nameCell.innerHTML = contact.name;
+	
+	var addressCell = row.insertCell();
+	addressCell.setAttribute('data-label', "Address");
+	addressCell.innerHTML = contact.address;
+	
+	var emailCell = row.insertCell();
+	emailCell.setAttribute('data-label', "Email");
+	emailCell.innerHTML = contact.email;
 }
